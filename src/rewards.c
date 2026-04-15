ede2264c5abc0e6882fac3785b20ee3e4ffff56d
Alberto Calo <albywiller@gmail.com>
2026-04-15 22:32:56 +0100

    Switched to queue pending reward stages to prevent milestone skips

--- src/rewards.c
+++ src/rewards.c
@@ -14,13 +14,14 @@ static const uint16_t kRewardProgressMilestones[REWARD_STAGE_COUNT] = {5, 10, 15
 
 static uint16_t reward_progress = 0;
 static uint8_t unlocked_reward_stage = 0;
-static uint8_t pending_reward_stage = 0;
+/* Next stage to consume; acts as an ordered pending queue cursor. */
+static uint8_t next_pending_stage = 1;
 
 void rewards_reset(void)
 {
     reward_progress = 0;
     unlocked_reward_stage = 0;
-    pending_reward_stage = 0;
+    next_pending_stage = 1;
 }
 
 void rewards_register_hand_result(uint16_t win_amount)
@@ -34,33 +35,38 @@ void rewards_register_hand_result(uint16_t win_amount)
     while (unlocked_reward_stage < REWARD_STAGE_COUNT &&
            reward_progress >= kRewardProgressMilestones[unlocked_reward_stage]) {
         unlocked_reward_stage++;
-        pending_reward_stage = unlocked_reward_stage;
     }
 }
 
 uint8_t rewards_has_pending(void)
 {
-    return (pending_reward_stage > 0);
+    return (next_pending_stage <= unlocked_reward_stage);
 }
 
 uint8_t rewards_pending_stage(void)
 {
-    return pending_reward_stage;
+    if (next_pending_stage <= unlocked_reward_stage) {
+        return next_pending_stage;
+    }
+    return 0;
 }
 
 uint8_t rewards_is_final_stage_pending(void)
 {
-    return (pending_reward_stage == REWARD_STAGE_COUNT);
+    return (next_pending_stage == REWARD_STAGE_COUNT &&
+            next_pending_stage <= unlocked_reward_stage);
 }
 
 uint8_t rewards_consume_pending_stage(void)
 {
-    uint8_t stage = pending_reward_stage;
-    pending_reward_stage = 0;
+    uint8_t stage = rewards_pending_stage();
+    if (stage > 0 && next_pending_stage <= REWARD_STAGE_COUNT) {
+        next_pending_stage++;
+    }
     return stage;
 }
 
 void rewards_clear_pending(void)
 {
-    pending_reward_stage = 0;
+    next_pending_stage = (uint8_t)(unlocked_reward_stage + 1);
 }
